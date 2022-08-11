22d5a12342732c96a28a5edb8fc4ae585b41ccfe
Richard Huffman <richardhuffman96@gmail.com>
2022-08-11 01:27:13 -0400

    finally got the damn thing working. is now somewhat usable. to deploy soon

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bar-book', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
